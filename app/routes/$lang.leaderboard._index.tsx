4b81a7ed8447f295026c17091fc68dc5e25ef19e
Jame Zhan <jame@orderly.network>
2026-01-31 01:35:09 +0800

    feat: update leaderboard page

--- app/routes/$lang.leaderboard._index.tsx
+++ app/routes/$lang.leaderboard._index.tsx
@@ -1,79 +1,16 @@
-import { useMemo } from "react";
 import { MetaFunction } from "@remix-run/node";
-import { i18n, parseI18nLang } from "@orderly.network/i18n";
-import {
-  Campaign,
-  LeaderboardWidget,
-} from "@orderly.network/trading-leaderboard";
-import { Box, useScreen } from "@orderly.network/ui";
-import { useScaffoldContext } from "@orderly.network/ui-scaffold";
+import { GeneralLeaderboardWidget } from "@orderly.network/trading-leaderboard";
 import { PageTitleMap, PathEnum } from "@/constant";
-import { getSymbol } from "@/storage";
 import { generatePageTitle } from "@/utils";
 
 export const meta: MetaFunction = () => {
   return [{ title: generatePageTitle(PageTitleMap[PathEnum.Leaderboard]) }];
 };
 
-function getCampaigns() {
-  const addDays = (date: Date, days: number) => {
-    const result = new Date(date);
-    result.setDate(result.getDate() + days);
-    return result;
-  };
-
-  const dateRange = [
-    // ongoing
-    { startTime: addDays(new Date(), -1), endTime: addDays(new Date(), 30) },
-    // future
-    { startTime: addDays(new Date(), 1), endTime: addDays(new Date(), 30) },
-    // past
-    { startTime: addDays(new Date(), -30), endTime: addDays(new Date(), -1) },
-  ];
-
-  return dateRange.map(
-    (date) =>
-      ({
-        title: "RISE ABOVE. OUTTRADE THE REST",
-        description:
-          "A new era of traders is rising. Are you the one leading the charge? Compete for your share of $10K by climbing the ranks. Only the bold will make it to the top.",
-        image: "/images/leaderboard/campaign.jpg",
-        href: "https://orderly.network/",
-        ...date,
-      }) as Campaign,
-  );
-}
-
-export default function MarketsPage() {
-  const { isMobile } = useScreen();
-  const { topNavbarHeight, footerHeight, announcementHeight } =
-    useScaffoldContext();
-
-  const tradingUrl = useMemo(() => {
-    const symbol = getSymbol();
-    return `/${parseI18nLang(i18n.language)}${PathEnum.Perp}/${symbol}`;
-  }, []);
-
+export default function LeaderboardPage() {
   return (
-    <Box
-      style={{
-        minHeight: 379,
-        maxHeight: 2560,
-        overflow: "hidden",
-        height: isMobile
-          ? "100%"
-          : `calc(100vh - ${topNavbarHeight}px - ${footerHeight}px - ${
-              announcementHeight ? announcementHeight + 12 : 0
-            }px)`,
-      }}
-    >
-      <LeaderboardWidget
-        campaigns={getCampaigns()}
-        href={{
-          trading: tradingUrl,
-        }}
-        className="oui-py-5"
-      />
-    </Box>
+    <div className="oui-py-6 oui-px-4 lg:oui-px-6 xl:oui-pl-4 lx:oui-pr-6">
+      <GeneralLeaderboardWidget />
+    </div>
   );
 }
